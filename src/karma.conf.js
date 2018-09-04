6693e30f09bcb544b0a6210a742a4486391dd95a
Ainsley <ab.chang@me.com>
2018-09-04 10:16:55 +0100

    use headless chrome for tests

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,15 +1,15 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+// Use puppeteer
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
 module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      'karma-*',
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
@@ -25,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false
   });
 };
